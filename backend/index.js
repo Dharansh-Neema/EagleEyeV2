9d7bfb2eb0e937cc056ac652e765ee6fdd6203dd
Dharansh-Neema <dharanshneema@gmail.com>
2025-06-17 13:44:32 +0530

    Index file changed to add cookiee function

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const morgan = require('morgan');
 const { connectDB } = require('./config/db');
 require('dotenv').config();
-
+const cookieParser = require('cookie-parser');
 // Import routes
-// const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // Initialize express app
 const app = express();
@@ -16,9 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 // Routes
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 // Root route
 app.get('/', (req, res) => {
