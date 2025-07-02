6d4da1ef398b4f3584979b6fa52658862fac7082
Dharansh-Neema <dharanshneema@gmail.com>
2025-07-02 15:32:34 +0530

    model updated

--- backend/models/imageModel.js
+++ backend/models/imageModel.js
@@ -1,4 +1,4 @@
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 /*
  Image Schema
@@ -21,15 +21,19 @@ const imageSchema = {
   project: { type: Object, required: true },
   inspection_station: { type: Object, required: true },
   camera: { type: Object, required: true },
+
+  ground_truth: { type: Object, required: false },
+  inference: { type: Object, required: false },
   full_path: { type: String, required: true },
   created_at: { type: Date, default: () => new Date() },
-  updated_at: { type: Date, default: () => new Date() }
+  updated_at: { type: Date, default: () => new Date() },
 };
 
 function validateImage(data) {
   const errors = {};
   for (const [field, cfg] of Object.entries(imageSchema)) {
-    if (cfg.required && data[field] === undefined) errors[field] = `${field} is required`;
+    if (cfg.required && data[field] === undefined)
+      errors[field] = `${field} is required`;
   }
   return { isValid: Object.keys(errors).length === 0, errors };
 }
@@ -40,7 +44,8 @@ async function createImage(db, data) {
 
   for (const [field, cfg] of Object.entries(imageSchema)) {
     if (cfg.default !== undefined && data[field] === undefined) {
-      data[field] = typeof cfg.default === 'function' ? cfg.default() : cfg.default;
+      data[field] =
+        typeof cfg.default === "function" ? cfg.default() : cfg.default;
     }
   }
 
@@ -50,37 +55,54 @@ async function createImage(db, data) {
   data.inspection_station.id = new ObjectId(data.inspection_station.id);
   data.camera.id = new ObjectId(data.camera.id);
 
-  const result = await db.collection('images').insertOne(data);
-  return await db.collection('images').findOne({ _id: result.insertedId });
+  const result = await db.collection("images").insertOne(data);
+  return await db.collection("images").findOne({ _id: result.insertedId });
 }
 
 async function findImageById(db, id) {
-  return await db.collection('images').findOne({ _id: new ObjectId(id) });
+  return await db.collection("images").findOne({ _id: new ObjectId(id) });
 }
 
 async function getAllImages(db) {
-  return await db.collection('images').find().toArray();
+  return await db.collection("images").find().toArray();
 }
 
 async function findImagesForUser(db, orgIds) {
   const ids = orgIds.map((id) => new ObjectId(id));
-  return await db.collection('images').find({ 'organization.id': { $in: ids } }).toArray();
+  return await db
+    .collection("images")
+    .find({ "organization.id": { $in: ids } })
+    .toArray();
 }
 
 async function findImagesByCameraId(db, cameraId) {
   return await db
-    .collection('images')
-    .find({ 'camera.id': new ObjectId(cameraId) })
+    .collection("images")
+    .find({ "camera.id": new ObjectId(cameraId) })
     .toArray();
 }
 
 async function deleteImage(db, id) {
-  const res = await db.collection('images').deleteOne({ _id: new ObjectId(id) });
+  const res = await db
+    .collection("images")
+    .deleteOne({ _id: new ObjectId(id) });
   return res.deletedCount > 0;
 }
 
 async function countImages(db, query) {
-  return await db.collection('images').countDocuments(query);
+  return await db.collection("images").countDocuments(query);
+}
+
+async function updateGroundTruth(db, id, data) {
+  return await db
+    .collection("images")
+    .updateOne({ _id: new ObjectId(id) }, { $set: { ground_truth: data } });
+}
+
+async function updateInference(db, id, data) {
+  return await db
+    .collection("images")
+    .updateOne({ _id: new ObjectId(id) }, { $set: { inference: data } });
 }
 
 module.exports = {
@@ -90,5 +112,5 @@ module.exports = {
   findImagesForUser,
   findImagesByCameraId,
   deleteImage,
-  countImages
+  countImages,
 };
