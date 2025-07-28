6ab96b8b7be387f5949df7a294bb966c7631f4b8
Dharansh-Neema <dharanshneema@gmail.com>
2025-07-28 23:14:33 +0530

    CORS will be handled by ngnix

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 const { connectDB } = require("./config/db");
 require("dotenv").config();
@@ -20,7 +19,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
