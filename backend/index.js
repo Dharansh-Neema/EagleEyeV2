992d685632b5d00c882f935a7a21083148141c6a
Dharansh-Neema <dharanshneema@gmail.com>
2025-07-28 21:54:21 +0530

    CROS Policy changed

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = ['https://eagleeyed.netlify.app'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
